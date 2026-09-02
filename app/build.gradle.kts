5ed7c2bf8253a1b7b743ecfddd9fb388ea6cf9f0
EgorAL15 <alexanderovesar56789@gmail.com>
2026-09-02 17:55:39 +0300

    Initialize Gradle project

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
