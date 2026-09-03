734f264449c755bdee4d1b41fb8eeb1c4bab884a
EgorAL15 <alexanderovesar56789@gmail.com>
2026-09-03 10:35:47 +0300

    Add Jte templates and Tailwind CSS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,12 +18,17 @@ java {
 
 dependencies {
     implementation("io.javalin:javalin:7.2.3")
+    implementation("io.javalin:javalin-rendering-jte:7.2.3")
+    implementation("gg.jte:jte:3.2.4")
+
     implementation("org.slf4j:slf4j-simple:2.0.17")
 
+    // JCB-зависимости оставляем
     implementation("com.zaxxer:HikariCP:6.3.1")
     implementation("com.h2database:h2:2.3.232")
     implementation("org.postgresql:postgresql:42.7.7")
 
+    // тестовые зависимости тоже оставляем
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,24 @@
 package hexlet.code;
 
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
 import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+import io.javalin.rendering.template.JavalinJte;
 
 public class App {
 
     private static final int DEFAULT_PORT = 7070;
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver =
+                new ResourceCodeResolver("templates", classLoader);
+
+        return TemplateEngine.create(codeResolver, ContentType.Html);
+    }
 
     public static Javalin getApp() {
         var dataSource = Database.getDataSource();
@@ -14,8 +27,18 @@ public class App {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
 
+            config.fileRenderer(
+                    new JavalinJte(createTemplateEngine())
+            );
+
+            config.staticFiles.add(staticFiles -> {
+                staticFiles.hostedPath = "/";
+                staticFiles.directory = "/static";
+                staticFiles.location = Location.CLASSPATH;
+            });
+
             config.routes.get("/", ctx -> {
-                ctx.result("Hello World");
+                ctx.render("index.jte");
             });
         });
 
